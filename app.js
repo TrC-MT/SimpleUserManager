19b933639a2c1d669462e4ce381ecaddabf0b53d
Travis Cook <travis.cook5583@stu.mtec.edu>
2023-01-06 08:42:55 -0700

    Rendering userListing properly.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const jsonfile = require('jsonfile');
 const file = './users.json';
 const app = express();
 const port = 3000;
+app.set('views', './public/pages');
+app.set('view engine', 'pug');
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.static('public'));
@@ -12,12 +14,8 @@ app.use(express.static('public'));
 app.get('/', (req, res) => { //When the client goes to localhost:3000
     res.sendFile(`${__dirname}/public/pages/index.html`); //Send them index_html
 });
-app.get('/newUser.html', (req, res) => {
+app.get('/newUser', (req, res) => {
     res.sendFile(`${__dirname}/public/pages/newUser.html`)
-})
-//userListing down below
-app.get('/userEdit.html', (req, res) => {
-    res.sendFile(`${__dirname}/public/pages/userEdit.html`)
 })
 
 //=================================================
@@ -55,7 +53,8 @@ app.post('/addUser', (req, res) => { //When the submit button is clicked in the 
     jsonfile.writeFileSync(file, data, {spaces: 2}) //Write the new object to users.json
     console.log('Written to file.')
 
-    res.sendFile(`${__dirname}/public/pages/userListing.html`) //needs to emit get not just send the file !!!!!!!!!!!!!!!!!!!
+    res.redirect(`/userListing`)
+    // window.location.replace(`${__dirname}/public/pages/userListing.html`)
 }); //Closing app.post
 
 //------------------------------------------------------
@@ -66,24 +65,18 @@ app.post('/editUser', (req, res) => { //When the submit button is clicked in the
 
 });
 //-------------------------
-app.get('/userListing.html', (req, res) => { //When the client goes to userListing
-    jsonfile.readFileSync(file).then((data) => { //Read the users.json
-        let array = data.users;
-        let table = document.getElementById('userinformation');
-        let new_HTML = `<tr><th>Username</th><th>Name</th><th>Email</th><th>Age</th><th>Edit</th><th>Delete</th></tr>`;
-        array.forEach(user => { //For each user in the object
-            //Rendering will go here
-            //user.id, user.username, user.name, etc.
-            new_HTML+= `<tr>
-            <td>${user.username}</td>
-            <td>${user.name}</td>
-            <td>${user.email}</td>
-            <td>${user.age}</td>
-            <td><button onclick="edit(${user.id})">EDIT</button></td>
-            <td><button onclick="del(${user.id})">DELETE</button></td>`
-        });
-    });
+app.get('/userListing', (req, res) => { //When the client goes to userListing
+    
+    let data = jsonfile.readFileSync(file); //Read the users.json
+    res.render(`${__dirname}/public/pages/userListing.pug`, {users: data.users})
+
 });
+//----------------
+app.get('/userEdit/:id', (req, res) => {
+    //get the info from table
+    let id = req.params.id
+    res.render(`${__dirname}/public/pages/userEdit.pug`, {user: id}) //send the info to pug
+})
 //==================================
 
 function edit(id){
