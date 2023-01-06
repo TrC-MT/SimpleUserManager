cc6aebc2b3ad27b2c41f6d2860f0e225a1cdd257
Travis Cook <travis.cook5583@stu.mtec.edu>
2023-01-06 09:16:07 -0700

    Edit user now works.

--- app.js
+++ app.js
@@ -26,8 +26,6 @@ app.post('/addUser', (req, res) => { //When the submit button is clicked in the 
     const user_email = req.body.email;
     const user_age = req.body.age;
 
-    console.log('user_username: ' + user_username)
-
     let user_count = 0;
 
     let data = jsonfile.readFileSync(file) //Read the users.json
@@ -35,7 +33,6 @@ app.post('/addUser', (req, res) => { //When the submit button is clicked in the 
     array.forEach(user => { //For each user in the object
         user_count += 1
     });
-    console.log('user_count: ' + user_count)
 
     let new_user = {
         "id": `${user_count}`,
@@ -44,27 +41,18 @@ app.post('/addUser', (req, res) => { //When the submit button is clicked in the 
         "email": `${user_email}`,
         "age": `${user_age}`
     }; //Place those values into the new_user string object
-    console.log('new_user: ' + new_user)
+
 
     data.users[user_count] = new_user; //Place the new_user into the next index of the users array
 
-    console.log('data: ' + JSON.stringify(data))
-
     jsonfile.writeFileSync(file, data, {spaces: 2}) //Write the new object to users.json
-    console.log('Written to file.')
 
     res.redirect(`/userListing`)
     // window.location.replace(`${__dirname}/public/pages/userListing.html`)
 }); //Closing app.post
 
 //------------------------------------------------------
-app.post('/editUser', (req, res) => { //When the submit button is clicked in the userEdit.html
-    jsonfile.readFileSync(file).then((data) => {
-        
-    });
 
-});
-//-------------------------
 app.get('/userListing', (req, res) => { //When the client goes to userListing
     
     let data = jsonfile.readFileSync(file); //Read the users.json
@@ -73,21 +61,43 @@ app.get('/userListing', (req, res) => { //When the client goes to userListing
 });
 //----------------
 app.get('/userEdit/:id', (req, res) => {
-    //get the info from table
-    let id = req.params.id
-    res.render(`${__dirname}/public/pages/userEdit.pug`, {user: id}) //send the info to pug
+    let id = req.params.id //get the id from the link
+    let data = jsonfile.readFileSync(file); //Read the users.json
+    let user_wanted = null;
+    data.users.forEach(user => {
+        if(user.id == id){
+            user_wanted = user;
+        }
+    })
+    res.render(`${__dirname}/public/pages/userEdit.pug`, {user: user_wanted}) //send the info to pug
 })
+//-----
+app.post('/editUser', (req, res) => { //When the submit button is clicked in the userEdit.pug
+    //Grab the values from the form
+    const user_username = req.body.username;
+    const user_name = req.body.name;
+    const user_email = req.body.email;
+    const user_age = req.body.age;
+    const user_id = req.body.id
+
+    let edited_user = {
+        "id": `${user_id}`,
+        "username": `${user_username}`,
+        "name": `${user_name}`,
+        "email": `${user_email}`,
+        "age": `${user_age}`
+    }; //Place those values into the edited_user string object
+
+    let data = jsonfile.readFileSync(file); //Read the users.json
+    data.users[user_id] = edited_user; //Place the edited_user into the next index of the users array
+
+    jsonfile.writeFileSync(file, data, {spaces: 2}) //Write the new object to users.json
+
+    res.redirect(`/userListing`)
+
+});
 //==================================
 
-function edit(id){
-
-}
-
-function del(id){
-
-}
-
-//=============================================
 app.listen(port, () => {
     console.log(`Listening on port ${port} \n`);
 });
